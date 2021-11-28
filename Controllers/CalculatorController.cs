c17cbee6c5879683aa384c42140b3c3315390343
kiniomote <dimon.popadin.21@gmail.com>
2021-11-28 16:58:48 +0300

    Init

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1.Controllers
+{
+    [Route("api/calculator")]
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
